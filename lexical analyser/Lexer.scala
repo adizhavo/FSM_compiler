aecf5df2ce897ad2390e5a48c79301d0681f1833
Adi Zhavo <adi@gram.gs>
2017-01-08 22:06:26 +0000

    Bug fix in the lexer

--- lexical analyser/Lexer.scala
+++ lexical analyser/Lexer.scala
@@ -1,18 +1,15 @@
 package FSMC
-
 import scala.util.matching.Regex
 
 class Lexer {
-	val emptySpace = " "
 	val whiteSpaceRegex = new Regex("\\s+")
 	val nameRegex = new Regex("\\w+")
 
 	var lineNumber : Int = 1
 	var postionInLine : Int = 0
 
-	def Lex(fileStream : String) {
+	def Lex(lines : Iterator[String]) {
 		lineNumber = 1
-		var lines = fileStream.split(emptySpace)
 		for (line <- lines) {
 			lexLine(line)
 			lineNumber += 1
@@ -20,14 +17,15 @@ class Lexer {
 	}
 
 	private def lexLine(line : String) {
-		for (postionInLine <- 0 to line.length()) {
+		postionInLine = 0
+		while (postionInLine < line.length()) {
 			lexToken(line)
 		}
 	}
 
 	private def lexToken(line : String) = {
 		if (!findToken(line)) {
-			println ("notify token collector for error in line: " + lineNumber + " and position: " + postionInLine)
+			println ("notify token collector for error in line: " + lineNumber + " and position: " + postionInLine + " " + line.length())
 			postionInLine += 1
 		}
 	}
@@ -39,37 +37,49 @@ class Lexer {
 	}
 
 	private def findWhiteSpace(line : String) = {
-		val matches = whiteSpaceRegex.findPrefixOf(line.substring(postionInLine)).get.length()
-		if (matches > 0) {
-			postionInLine += matches
-			true
-		}
-		else false
+		if (postionInLine < line.length()) {
+			val foundWhiteSpace = whiteSpaceRegex.findPrefixOf(line.substring(postionInLine)).size > 0
+			if (foundWhiteSpace) {
+				val matches = whiteSpaceRegex.findPrefixOf(line.substring(postionInLine)).get.length()
+				if (matches > 0) {
+					postionInLine += matches
+					true
+				} else false
+			} else false
+		} else false
 	}
 
 	private def findSingleCharacterToken(line : String) = {
-		val char = line.substring(postionInLine, postionInLine + 1)
-		char match {
-			case "{" => println("notify token collector with: " + char) 
-			case "}" => println("notify token collector with: " + char) 
-			case ":" => println("notify token collector with: " + char) 
-			case ">" => println("notify token collector with: " + char) 
-			case "<" => println("notify token collector with: " + char) 
-			case default => false
+		if (postionInLine < line.length()) {
+			val char = line.substring(postionInLine, postionInLine + 1)
+			var notFound = false
+			char match {
+				case "{" => println("notify token collector with: " + char) 
+				case "}" => println("notify token collector with: " + char) 
+				case ":" => println("notify token collector with: " + char) 
+				case ">" => println("notify token collector with: " + char) 
+				case "<" => println("notify token collector with: " + char) 
+				case default => notFound = true
+			}
+			if (notFound) false
+			else {
+				postionInLine += 1
+				true
+			}
 		}
-
-		postionInLine += 1
-		true
+		else false
 	}
 
 	private def findName(line : String) = {
-		val found = nameRegex.findPrefixOf(line.substring(postionInLine)).size > 0
-		if (found) {
-			val name = nameRegex.findPrefixOf(line.substring(postionInLine)).get
-			println("notify token collector with: " + name + " name")
-			postionInLine += name.length()
-			true
-		}
-		else false
+		if (postionInLine < line.length()) {
+			val found = nameRegex.findPrefixOf(line.substring(postionInLine)).size > 0
+			if (found) {
+				val name = nameRegex.findPrefixOf(line.substring(postionInLine)).get
+				println("notify token collector with: " + name + " name at line: " + lineNumber + " and position: " + postionInLine)
+				postionInLine += name.length()
+				true
+			}
+			else false
+		} else false
 	}
 }
