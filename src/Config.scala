198d4a64e594eea36da9bc54598da72eaa10027c
Adi Zhavo <adi@gram.gs>
2017-01-19 16:51:02 +0000

    Execute CSharp visitor to generate C# code

--- src/Config.scala
+++ src/Config.scala
@@ -1,9 +1,35 @@
 package FSMC
 
+import java.io._
+
 object Config {
   val FSMHeader = "FSM"
   val HeaderInitialState = "Initial"
   val HeaderActionType = "Actions"
   val EmptyAction = "EMPTY_ACTION"
-  val OutputFolder = "output"
+  val JsonOutputFolder = "output"
+  val GeneratedCodeFolder = "generated"
+
+  // Add visitors IDs here
+  // -----
+  val CSharpVisitor = "C#"
+
+  // -----
+
+  def RequestVisitor (visitorCode : String) : LanguageVisitor = {
+		visitorCode match {
+			case Config.CSharpVisitor => return new CSharpVisitor()
+			case default => return null
+    }
+	}
+}
+
+object Utils {
+  def Output(path : String, text : String, message : String = "") {
+		val file = new File(path)
+		val bw = new BufferedWriter(new FileWriter(file))
+		bw.write(text)
+		println(message)
+		bw.close()
+	}
 }
